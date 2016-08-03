b676298ea1526362b674033e603ea3566018549b
Arkatakor <zakariel76@gmail.com>
2016-08-03 15:54:20 +0200

    update shrine to go by class instead of type so that mayan shrine can be included

--- BalancedAncientTechTree/Buildings/BuildingUpdates.sql
+++ BalancedAncientTechTree/Buildings/BuildingUpdates.sql
@@ -1,2 +1,2 @@
 --	Updates
-UPDATE Buildings SET PrereqTech = 'TECH_MYSTICISM' WHERE Type = 'BUILDING_SHRINE';
+UPDATE Buildings SET PrereqTech = 'TECH_MYSTICISM' WHERE BuildingClass = 'BUILDINGCLASS_SHRINE';
