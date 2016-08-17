2bc3cd36762d7f79ea8503de560e86687fbe8daa
Arkatakor <zakariel76@gmail.com>
2016-08-17 18:59:51 +0200

    attempts to make lua work (incomplete)

---
+++ Build/Ancient Techs Rebalanced/Techs/TechCoordinates.sql
@@ -0,0 +1,5 @@
+--	Relocate tech coordinates
+UPDATE Technologies Set GridY = 1 WHERE Type = 'TECH_AGRICULTURE';
+UPDATE Technologies Set GridY = 4 WHERE Type = 'TECH_ARCHERY';
+UPDATE Technologies Set GridY = 3 WHERE Type = 'TECH_ANIMAL_HUSBANDRY';
+
--- Build/Ancient Techs Rebalanced/Techs/TechsPrereq.sql
+++ Build/Ancient Techs Rebalanced/Techs/TechsPrereq.sql
@@ -5,16 +5,15 @@ UPDATE Builds SET PrereqTech = 'TECH_FISHING' WHERE Type = 'BUILD_FISHING_BOATS'
 --	Update sailing tech prerequisite to fishing instead of pottery
 UPDATE Technology_PrereqTechs Set PrereqTech = 'TECH_FISHING' WHERE TechType = 'TECH_SAILING';
 
-
 --	hunting
 UPDATE Builds SET PrereqTech = 'TECH_HUNTING' WHERE Type = 'BUILD_CAMP';
 
 --	Add prereq tech
-INSERT INTO Technology_PrereqTechs (TechType, PrereqTech) VALUES ('TECH_TRAPPING', 'TECH_HUNTING');
+--	INSERT INTO Technology_PrereqTechs (TechType, PrereqTech) VALUES ('TECH_TRAPPING', 'TECH_HUNTING');
 
 --	Add prereq tech for writing
 INSERT INTO Technology_PrereqTechs (TechType, PrereqTech) VALUES ('TECH_WRITING', 'TECH_MYSTICISM');
 
-UPDATE Technology_PrereqTechs Set PrereqTech = '' WHERE TechType = 'TECH_MINING';
 UPDATE Technology_PrereqTechs Set PrereqTech = 'TECH_HUNTING' WHERE TechType = 'TECH_ARCHERY';
 UPDATE Technology_PrereqTechs Set PrereqTech = 'TECH_STONE_TOOLS' WHERE TechType = 'TECH_MINING';
+
