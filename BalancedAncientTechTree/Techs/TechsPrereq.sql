f594f51bef960d8effa1b2a8634b54b463603a29
fabho992 <fabho992@RUD-D11015>
2017-11-20 16:02:56 +0100

    improving documentation and categorization of prereq techs

--- BalancedAncientTechTree/Techs/TechsPrereq.sql
+++ BalancedAncientTechTree/Techs/TechsPrereq.sql
@@ -1,31 +1,47 @@
--- Insert SQL Rules Here 
---	fishing
+-------------------------------------------------------
+--	FISHING
+-------------------------------------------------------
 UPDATE Builds SET PrereqTech = 'TECH_FISHING' WHERE Type = 'BUILD_FISHING_BOATS';
 
---	Update sailing tech prerequisite to fishing instead of pottery
-UPDATE Technology_PrereqTechs Set PrereqTech = 'TECH_FISHING' WHERE TechType = 'TECH_SAILING';
-
---	Hunting requires animal husbandry
-DELETE FROM Technology_PrereqTechs WHERE TechType = 'TECH_ANIMAL_HUSBANDRY';
-INSERT INTO Technology_PrereqTechs (TechType, PrereqTech) VALUES ('TECH_ANIMAL_HUSBANDRY', 'TECH_HUNTING');
-
+-------------------------------------------------------
+--	HUNTING
+-------------------------------------------------------
 --	hunting
 UPDATE Builds SET PrereqTech = 'TECH_HUNTING' WHERE Type = 'BUILD_CAMP';
 
---	Add prereq tech
---	INSERT INTO Technology_PrereqTechs (TechType, PrereqTech) VALUES ('TECH_TRAPPING', 'TECH_HUNTING');
+-------------------------------------------------------
+--	SAILING
+-------------------------------------------------------
+--	Update sailing tech prerequisite to fishing instead of pottery
+UPDATE Technology_PrereqTechs Set PrereqTech = 'TECH_FISHING' WHERE TechType = 'TECH_SAILING';
 
---	Add prereq tech for writing
+-------------------------------------------------------
+--	ANIMAL HUSBANDRY
+-------------------------------------------------------
+DELETE FROM Technology_PrereqTechs WHERE TechType = 'TECH_ANIMAL_HUSBANDRY';
+INSERT INTO Technology_PrereqTechs (TechType, PrereqTech) VALUES ('TECH_ANIMAL_HUSBANDRY', 'TECH_HUNTING');
+
+-------------------------------------------------------
+--	WRITING
+-------------------------------------------------------
 INSERT INTO Technology_PrereqTechs (TechType, PrereqTech) VALUES ('TECH_WRITING', 'TECH_MYSTICISM');
 
+-------------------------------------------------------
+--	ARCHERY
+-------------------------------------------------------
 UPDATE Technology_PrereqTechs Set PrereqTech = 'TECH_HUNTING' WHERE TechType = 'TECH_ARCHERY';
+
+-------------------------------------------------------
+--	MINING
+-------------------------------------------------------
 UPDATE Technology_PrereqTechs Set PrereqTech = 'TECH_STONE_TOOLS' WHERE TechType = 'TECH_MINING';
 
---	Add prereq tech for the wheel
+-------------------------------------------------------
+--	THE WHEEL
+-------------------------------------------------------
 INSERT INTO Technology_PrereqTechs (TechType, PrereqTech) VALUES ('TECH_THE_WHEEL', 'TECH_WOOD_WORKING');
 
---	Add prereq tech for the archery
---INSERT INTO Technology_PrereqTechs (TechType, PrereqTech) VALUES ('TECH_ARCHERY', 'TECH_STONE_TOOLS');
-
---	Add stone tools as prerequisite for wookd working
+-------------------------------------------------------
+--	WOOD WORKING
+-------------------------------------------------------
 INSERT INTO Technology_PrereqTechs (TechType, PrereqTech) VALUES ('TECH_WOOD_WORKING', 'TECH_STONE_TOOLS');
