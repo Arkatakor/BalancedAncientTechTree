27a896501fdc9814c83ef6eb1e78997fa3bb7982
fabho992 <fabho992@RUD-D11015>
2017-12-13 16:14:10 +0100

    insertion of knosssos debuff "promotion"

--- BalancedAncientTechTree/Units/Updates.sql
+++ BalancedAncientTechTree/Units/Updates.sql
@@ -1,2 +1,6 @@
 UPDATE Units SET PrereqTech = 'TECH_FISHING' WHERE Class = 'UNITCLASS_WORKBOAT';
 UPDATE Units SET PrereqTech = 'TECH_HUNTING' WHERE Class = 'UNITCLASS_SCOUT';
+
+INSERT INTO UnitPromotions 
+			(Type, 								Description, 							Help, 										Sound, 				EnemyDamageChance,	ExtraAttacks,	CanMoveAfterAttacking,	EnemyDamage,	CannotBeChosen, PortraitIndex, 	IconAtlas, 			PediaType, 			PediaEntry)
+VALUES		('DEBUFF_KNOSSOS_DAMAGE', 			'TXT_KEY_DEBUFF_KNOSSOS_DAMAGE',		'TXT_KEY_DEBUFF_KNOSSOS_DAMAGE_HELP', 		'AS2D_IF_LEVELUP', 	100,					0,				0,						5,				1, 				57, 		'ABILITY_ATLAS', 	'PEDIA_ATTRIBUTES', 'TXT_KEY_DEBUFF_KNOSSOS_DAMAGE');
